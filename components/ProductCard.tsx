ed708ee0da4fbb55efc192061e457bb34459a4f0
scaleupmidul <scaleup.midul@gmail.com>
2025-11-25 17:43:09 +0600

    Update ProductCard.tsx

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,5 +1,4 @@
 
-
 import React, { useState, memo } from 'react';
 import { ArrowRight } from 'lucide-react';
 import { Product } from '../types';
@@ -29,8 +28,6 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
                     className={`object-cover w-full h-full transition-opacity duration-500 group-hover:scale-105 ${isImageLoaded ? 'opacity-100' : 'opacity-0'}`}
                     loading="lazy"
                     decoding="async"
-                    // @ts-ignore
-                    fetchpriority="low"
                     onLoad={() => setIsImageLoaded(true)}
                 />
                 <div className="absolute top-3 left-3 flex flex-col items-start space-y-1.5">
